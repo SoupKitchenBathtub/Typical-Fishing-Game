f579eaa0786437b3af80a270e57079e289edd734
SoupKitchenBathtub <loijac2018@gmail.com>
2025-04-15 23:45:08 -0500

    Added Player HUD basics

---
+++ Assets/_game/Scripts/Common/PlayerHudController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using EasyTransition;
+using UnityEngine;
+
+public class PlayerHudController : MonoBehaviour
+{
+
+
+
+}
